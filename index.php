ec3b9bac18ab5ac0c82bd0f76e016de429315812
Furini2002 <llfurini2002@gmail.com>
2024-07-03 21:42:41 -0300

    Cabecalho e rodape

--- index.php
+++ index.php
@@ -8,10 +8,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MovieStar</title>
+    <title>CineStar</title>
 
     <!--LINK DO LOGO-->
-    <link rel="short icon" href="img/MovieStar.ico">    
+    <link rel="short icon" href="<?= $BASE_URL ?>img/cinestar.ico">    
 
     <!--LINK DO BOOTSTRAP CSS-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/css/bootstrap.css" integrity="sha512-VcyUgkobcyhqQl74HS1TcTMnLEfdfX6BbjhH8ZBjFU9YTwHwtoRtWSGzhpDVEJqtMlvLM2z3JIixUOu63PNCYQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -20,13 +20,62 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <!--LINK DO CSS-->
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
 
 </head>
 <body>
+    <header>
+        <nav id="main-navbar" class="navbar navbar-expand-lg">
+            <a href="<?= $BASE_URL ?>" class="navbar-brand">
+                <img src="<?= $BASE_URL ?>img/logo.svg" alt="Logo CineStar" id="logo">
+                <span id="cinestar-title">CineStar</span>                
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="colapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fas fa-bars"></i>
+            </button>
+            <form action="" method="GET" id="search-form" class="form-inline my-2 my-lg-0">
+                <input type="text" name="q" id="search" class="form-control mr-sm-2" type="search" placeholder="Buscar Filmes" aria-label="Search">
+                <button class="btn my-2 my-sm-0" type="submit">
+                    <i class="fas fa-search"></i>
+                </button>
+            </form>
+            <div class="collapse navbar-collapse" id="navbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="<?= $BASE_URL ?>auth.php" class="nav-link">Entar / Cadastrar</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>    
 
-    <h1>123123</h1>
-    <i class="far fa-plus-square"></i>
+    <div id="main-container" class="container-fluid">
+        <h1>Corpo do site</h1>
+    </div>
+
+    <footer id="footer">
+        <div id="social-container">
+            <ul>
+                <li>
+                    <a href="#"><i class="fab fa-facebook-square"></i></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fab fa-youtube"></i></a>
+                </li>
+            </ul>
+        </div>
+        <div id="footer-links-container">
+            <ul>
+                <li><a href="#">Adicionar filme</a></li>
+                <li><a href="#">Adicionar crítica</a></li>
+                <li><a href="#">Entrar / Registrar</a></li>
+            </ul>
+        </div>
+        <p>&copy; 2024 Furini</p>
+    </footer>
     
     <!--LINK DO BOOTSTRAP JS-->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
