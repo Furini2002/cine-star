adfd8a8ff9cf18682d81603fb5b590f2d910f34c
Furini2002 <llfurini2002@gmail.com>
2024-07-12 22:17:03 -0300

    Inserindo filmes no banco

---
+++ movie_process.php
@@ -0,0 +1,90 @@
+<?php
+
+    require_once "models/Movie.php";
+    require_once "models/Message.php";
+    require_once "dao/MovieDAO.php";
+    require_once "dao/UserDAO.php";
+    require_once "globals.php";
+    require_once "db.php";
+
+    $message = new Message($BASE_URL);
+    $userDao = new UserDao($conn, $BASE_URL);   
+    $movieDao = new MovieDAO($conn, $BASE_URL);   
+
+    //VERIFICA O TIPO DE FORMULARIO
+    $type = filter_input(INPUT_POST, "type");  
+    
+    // resgat dados do usuario
+    $userData = $userDao->verifyToken();
+
+    if($type === "create"){        
+
+        //receber os dados do input
+        $title = filter_input(INPUT_POST, "title");
+        $description = filter_input(INPUT_POST, "description");
+        $trailer = filter_input(INPUT_POST, "trailer");
+        $category = filter_input(INPUT_POST, "category");
+        $length = filter_input(INPUT_POST, "length");
+    
+        //criar o objeto de usuario
+        $movie = new Movie();
+
+        //Validação minima de dados
+        if(!empty($title) && !empty($description) && !empty($category)){
+
+            $movie->title = $title;
+            $movie->description = $description;
+            $movie->trailer = $trailer;
+            $movie->category = $category;
+            $movie->length = $length;
+            $movie->users_id = $userData->id;
+
+            //upload de imagem do filme
+            if(isset($_FILES["image"]) && !empty($_FILES["image"]["tmp_name"])) {
+
+                $image = $_FILES["image"];
+                $imageTypes = ["image/jpeg", "image/jpg", "image/png"];
+                $jpgArray = ["image/jpeg", "image/jpg"];
+
+                //checando tipo da imagem
+                if(in_array($image["type"], $imageTypes)){
+
+                    // chaca se imagem é jpg
+                    if(in_array($image["type"], $jpgArray)){
+                        $imageFile = @imagecreatefromjpeg($image["tmp_name"]);
+                    }else{
+                        $imageFile = @imagecreatefrompng($image["tmp_name"]);
+                    } 
+
+                    //checa se conseguiu criar a imagem por meio do arquivo temporario
+                    if($imageFile === false) {
+                        $message->setMessage("Erro ao criar imagem a partir do arquivo temporário.", "error", "back");
+                    }
+            
+                    $imagename = $movie->imageGenerateName();
+
+                    //verifica se conseguiu gerar o nome
+                    if(!$imagename) {
+                        $message->setMessage("Erro ao gerar nome da imagem", "error", "back");
+                    }
+
+                    //verifica se conseguiu salvar no caminho especificado
+                    $savePath = "./img/movies/" . $imagename;
+                    if(!@imagejpeg($imageFile, $savePath, 100)) {
+                        $message->setMessage("Erro ao salvar no caminho especificado", "error", "back");
+                    }
+
+                    $movie->image = $imagename;
+
+                }else {
+                    $message->setMessage("Tipo inválido de imagem, insira png ou jpeg!", "error", "back");
+                }                
+            }
+            $movieDao->create($movie);
+        } else {
+            $message->setMessage("Você precisa adicionar ao menos: título, descrição e categoria", "error", "back");
+        }
+
+    } else{       
+        $message->setMessage("Informações inválidas!", "error", "back");
+    }
